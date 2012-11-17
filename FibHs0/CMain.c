869a7280d0eaaab60790a781faa880aadc926fd9
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-17 16:39:28 +0900

    remove space

--- FibHs0/CMain.c
+++ FibHs0/CMain.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "HsFFI.h"
-
 #ifdef __GLASGOW_HASKELL__
 #include "Fib_stub.h"
 #endif
@@ -10,11 +9,9 @@ int main(int argc, char *argv[])
 	int i;
 
 	hs_init(&argc, &argv);
-
 	for (i = 0; i < 30; i++) {
 		printf("%d\n", fib(i));
 	}
-
 	hs_exit();
 	return 0;
 }
