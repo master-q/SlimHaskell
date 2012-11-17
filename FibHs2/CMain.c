16dc78f913fd90a83b38311eab29381a1832b34f
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-18 02:54:20 +0900

    remove stat with ghc-arafura 0e745db57b06a1a332bbfd0ea02236163121f193

--- FibHs2/CMain.c
+++ FibHs2/CMain.c
@@ -30,5 +30,10 @@ void stopHeapProfTimer(void) {}
 void startHeapProfTimer(void) {}
 void awaitEvent(int wait) {}
 int getDelayTarget (int us) {usleep(us); return 0;}
-void heapCensus (double t) {}
+void heapCensus(double t) {}
 void ffi_call(void *cif, void (*fn)(), void *rvalue, void **avalue) {}
+void stat_startGC(void *cap, void *gct) {}
+void stat_endGC(void *cap, void *gct, size_t alloc, size_t live,
+    size_t copied, size_t slop, int gen, int par_n_threads,
+    size_t par_max_copied, size_t par_tot_copied) {}
+void stat_exit(int alloc) {}
