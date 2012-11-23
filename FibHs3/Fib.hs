2931ef0a3e046c8f2d21541873b597b4ab4f4a0f
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-23 06:31:12 +0000

    change include path

--- FibHs3/Fib.hs
+++ FibHs3/Fib.hs
@@ -3,5 +3,9 @@ import Foreign.C.Types
 
 foreign export ccall fib :: CInt -> IO CInt
 
+fibonacci :: [CInt]
+fibonacci = 1:1:zipWith (+) fibonacci (tail fibonacci)
+
 fib :: CInt -> IO CInt
-fib _ = return 0 -- Simplify
+fib n | 0 <= n && n <= 40 = return $ fibonacci !! fromIntegral n
+      | otherwise = return 0
