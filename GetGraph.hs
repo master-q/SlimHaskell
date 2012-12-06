3a0c8baad8e7a4d416f5a37337ee9d67f43e7b82
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-12-06 14:32:48 +0900

    Cleanup code.

--- GetGraph.hs
+++ GetGraph.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS -Wall #-}
 import Data.Char
+import Data.List
 import System.Process
 
 fibhss :: String
@@ -9,14 +10,33 @@ isUndefSym :: String -> Bool
 isUndefSym xs@(x:_) = isSpace x && head (filter isAlpha xs) == 'U'
 isUndefSym _     = False
 
+numSort :: String -> String -> Ordering
+numSort a b = a' `compare` b'
+  where
+    a', b' :: Int
+    a' = read $ filter isDigit a
+    b' = read $ filter isDigit b
+
+sizeMe :: String -> IO String
+sizeMe f = do
+  size <- readProcess "size" [f] ""
+  return $ (map words . lines $ size) !! 1 !! 3
+
+lddMe :: String -> IO String
+lddMe f = do
+  ldd <- readProcess "ldd" [f] ""
+  return $ show . length . lines $ ldd
+
+nmMe :: String -> IO String
+nmMe f = do
+  nm <- readProcess "nm" [f] ""
+  return $ show . length . filter isUndefSym . lines $ nm
+
 main :: IO ()
 main = do
-  -- size
-  size <- readProcess "size" [fibhss] ""
-  putStrLn $ (map words . lines $ size) !! 1 !! 3
-  -- ldd
-  ldd <- readProcess "ldd" [fibhss] ""
-  print . length . lines $ ldd
-  -- nm
-  nm <- readProcess "nm" [fibhss] ""
-  print $ length . filter isUndefSym . lines $ nm
+  -- get file paths
+  files <- readProcess "find" [".", "-name", "FibHs"] ""
+  print $ sortBy numSort . lines $ files
+  putStrLn =<< sizeMe fibhss
+  putStrLn =<< lddMe fibhss
+  putStrLn =<< nmMe fibhss
