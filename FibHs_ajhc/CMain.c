17662cd8382ba7d46e175c8f72584215e32ff58f
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-05-18 03:35:10 +0900

    Free gift: example ajhc code.

---
+++ FibHs_ajhc/CMain.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "HsFFI.h"
+#ifdef __GLASGOW_HASKELL__
+#include "Fib_stub.h"
+#endif
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	hs_init(&argc, &argv);
+	for (i = 0; i < 30; i++) {
+		printf("%d\n", fib(i));
+	}
+	hs_exit();
+	return 0;
+}
